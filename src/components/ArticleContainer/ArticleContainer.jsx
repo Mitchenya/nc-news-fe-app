734d1fcfc2eab9a6568bfc4b0b65c7041a61c6d0
Mitchenya <Mitchellrjshepherd@outlook.com>
2024-09-19 16:33:34 +0100

    This commit will add the option for the user to filter the articles via their topic

--- src/components/ArticleContainer/ArticleContainer.jsx
+++ src/components/ArticleContainer/ArticleContainer.jsx
@@ -5,16 +5,43 @@ import ArticleCard from "../ArticleCard/ArticleCard";
 
 function ArticleContainer() {
   const [articles, setArticles] = useState([]);
+  const [filteredArticles, setFilteredArticles] = useState([]);
 
   useEffect(() => {
     getAllArticles().then((data) => {
       setArticles(data.articles);
+      setFilteredArticles(data.articles);
     });
   }, []);
 
+  const handleButtons = (e) => {
+    const topic = e.target.value;
+
+    if (topic === "All") {
+      setFilteredArticles(articles);
+    } else {
+      const filtered = articles.filter((article) => article.topic === topic);
+      setFilteredArticles(filtered);
+    }
+  };
+
   return (
     <div className="article-container">
-      {articles.map((article) => {
+      <div className="buttons">
+        <button value="All" onClick={handleButtons}>
+          All
+        </button>
+        <button value="cooking" onClick={handleButtons}>
+          Cooking
+        </button>
+        <button value="coding" onClick={handleButtons}>
+          Coding
+        </button>
+        <button value="football" onClick={handleButtons}>
+          Football
+        </button>
+      </div>
+      {filteredArticles.map((article) => {
         return (
           <div key={article.article_id} className="article-card">
             <ArticleCard article={article} article_id={article.article_id} />
